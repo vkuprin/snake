ed7e7bd2a86461e058bc212e6fdd92ad3e24523f
Vitaly Kuprin <vitas_s@inbox.lv>
2025-08-21 00:52:30 +0300

    finalize deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/snake/" : "/",
+  base: "/",
   test: {
     globals: true,
     environment: "jsdom",
